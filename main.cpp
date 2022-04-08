af2c4f4c3047a267fc77e99cb8b8bde96a2415ce
Viktor <chromes08@yandex.ru>
2022-04-08 17:04:08 +0300

    Создание проекта

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "sources/collection/collectioninnovations.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    CollectionInnovations w;
+    w.show();
+    return a.exec();
+}
